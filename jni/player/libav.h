18f9d8eb06fd311140c3b08254acf4169274759e
chrulri <chrulri@gmail.com>
2012-05-13 01:46:04 +0200

    use macro for extern "C"

--- jni/player/libav.h
+++ jni/player/libav.h
@@ -1,14 +1,15 @@
 #ifndef DROIDTV_LIBAV_H
 #define DROIDTV_LIBAV_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
 #include "../libav/config.h"
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
-#ifdef __cplusplus
-}
-#endif
+
+__END_DECLS
+
 #endif // DROIDTV_LIBAV_H
